1c742c98a189f2d7f26c389bf6cbaf5a36f4fa18
Kevin Luedemann <kevin27luedemann@gmail.com>
2018-07-17 10:40:12 +0200

    RTC and HDC1080 do not work together

--- src/main.cpp
+++ src/main.cpp
@@ -31,13 +31,14 @@ void operator delete[](void * ptr)
 	free(ptr);
 }
 
-ds3231 rtc;
+//ds3231 rtc;
 hdc1080 sens;
 Input SQM(&DDRB,&PORTB,&PINB,0,true);
-uint8_t flag;
-#define CLOCK_TICK  0
+uint8_t flag_reg;
+#define CLOCK_TICK_BIT  0
 
 void output_time(char* out){
+    /*
     out[0] = '0'+rtc.t.mday/10;
     out[1] = '0'+rtc.t.mday%10;
     out[2] = '.';
@@ -61,6 +62,28 @@ void output_time(char* out){
     out[17] = '0'+rtc.t.sec/10;
     out[18] = '0'+rtc.t.sec%10;
     out[19] = ' ';
+    */
+    out[0] = '1';
+    out[1] = '7';
+    out[2] = '.';
+    out[3] = '0';
+    out[4] = '7';
+    out[5] = '.';
+    out[6] = '2';
+    out[7] = '0';
+    out[8] = '1';
+    out[9] = '8';
+
+    out[10] = 'T';
+    out[11] = '0';
+    out[12] = '9';
+    out[13] = ':';
+    out[14] = '0';
+    out[15] = '0';
+    out[16] = ':';
+    out[17] = '0';
+    out[18] = '0';
+    out[19] = ' ';
 
     float tempera   = sens.temperature();
     float humid     = sens.humidity();
@@ -72,7 +95,7 @@ void output_time(char* out){
     out[24] = '0'+(uint8_t)(tempera*10.)%10;
     out[25] = '0'+(uint8_t)(tempera*100.)%10;
     out[26] = 'C';
-    out[27] = '0'+(uint8_t)humid/10;
+    out[27] = '0'+(uint8_t)(humid/10);
     out[28] = '0'+(uint8_t)humid%10;
     out[29] = '.';
     out[30] = '0'+(uint8_t)(humid*10)%10;
@@ -81,60 +104,46 @@ void output_time(char* out){
     out[32] = '\r';
     out[33] = '\n';
     out[34] = '\0';
+    //if((uint8_t)humid/10>=10.){
+        //sens.reset();
+    //}
 }
 
 ISR(USART_RX_vect){
     uint8_t temp = uart_getc();
     if (temp == 't'){
-        rtc.get();
-        //sens.gettemphum();
-        char outpp[40];
-        output_time(outpp);
-        uart_puts(outpp);
+        flag_reg |= (1<<CLOCK_TICK_BIT);
+    }
+    else if(temp == 'p'){
+        uart_putc(temp+1);
     }
 }
 
-/*
-ISR(PCINT0_vect){
-    if(SQM.ison()){
-        flag |= (1<<CLOCK_TICK);
-    }
-}
-*/
 ISR(INT0_vect){
-    //flag |= (1<<CLOCK_TICK);
+    flag_reg |= (1<<CLOCK_TICK_BIT);
 }
 
 int main(void){
-    flag = 0;
+    flag_reg = 0;
     ACSR = (1<<ACD);
     uart_init();
-    rtc.deactivate_sqm();
+    //rtc.deactivate_sqm();
     EICRA |= (1<<ISC01);
     EIMSK |= (1<<INT0);
     sei();
 
     while(true){
-        /*
-        if(SQM.ison()){
-            rtc.get();
+        if(flag_reg&(1<<CLOCK_TICK_BIT)){
             sens.gettemphum();
+            //rtc.get();
             char outpp[40];
             output_time(outpp);
             uart_puts(outpp);
-            flag&=~(1<<CLOCK_TICK);
-            for(uint8_t i=0; i<10; i++){_delay_ms(50);}
-        }*/
-
-        if(flag&(1<<CLOCK_TICK)){
-            rtc.get();
-            sens.gettemphum();
-            char outpp[40];
-            output_time(outpp);
-            uart_puts(outpp);
-            flag&=~(1<<CLOCK_TICK);
+            flag_reg&=~(1<<CLOCK_TICK_BIT);
         }
-
+        else{
+            _delay_us(1);
+        }
 
     }
     return 0;
